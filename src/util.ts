bd96276488ebe06ee86c99f5c9981a6f69b7137a
SureshKuchana <kuchana123.suresh@gmail.com>
2023-01-01 22:21:34 +0530

    fix assests not loading

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,14 @@
+import img1 from './assets/avatars/daniel.jpg';
+import img2 from './assets/products/image-aqua.png';
+import img3 from './assets/products/image-rose.png';
+import img33 from './assets/avatars/kristy.png';
+
+import img4 from './assets/products/image-steel.png';
+import img44 from './assets/avatars/veronika.jpg';
+
+import img5 from './assets/products/image-yellow.png';
+import img55 from './assets/avatars/molly.png';
+
 import { Products } from './types';
 
 export const generateVoteCount = () => {
@@ -13,8 +24,8 @@ export const productsData: Products[] = [
   {
     description: 'On-demand sand castle construction expertise.',
     id: 1,
-    productImageUrl: 'images/products/image-aqua.png',
-    submitterAvatarUrl: 'images/avatars/daniel.jpg',
+    productImageUrl: img2,
+    submitterAvatarUrl: img1,
     title: 'Yellow Pail',
     url: '#',
     votes: generateVoteCount(),
@@ -22,8 +33,8 @@ export const productsData: Products[] = [
   {
     description: 'Earn points when your favorite politicians pass legislation.',
     id: 2,
-    productImageUrl: 'images/products/image-rose.png',
-    submitterAvatarUrl: 'images/avatars/kristy.png',
+    productImageUrl: img3,
+    submitterAvatarUrl: img33,
     title: 'Supermajority: The Fantasy Congress League',
     url: '#',
     votes: generateVoteCount(),
@@ -31,8 +42,8 @@ export const productsData: Products[] = [
   {
     description: 'We already have your measurements and shipping address.',
     id: 3,
-    productImageUrl: 'images/products/image-steel.png',
-    submitterAvatarUrl: 'images/avatars/veronika.jpg',
+    productImageUrl: img4,
+    submitterAvatarUrl: img44,
     title: 'Tinfoild: Tailored tinfoil hats',
     url: '#',
     votes: generateVoteCount(),
@@ -40,8 +51,8 @@ export const productsData: Products[] = [
   {
     description: 'High-minded or absent-minded? You decide.',
     id: 4,
-    productImageUrl: 'images/products/image-yellow.png',
-    submitterAvatarUrl: 'images/avatars/molly.png',
+    productImageUrl: img5,
+    submitterAvatarUrl: img55,
     title: 'Haught or Naught',
     url: '#',
     votes: generateVoteCount(),
